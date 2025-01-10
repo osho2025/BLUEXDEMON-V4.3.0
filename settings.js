08fe8227256e3f7e6ac14c168838b4e111bcbab7
osho2025 <bellodivine135@gmail.com>
2025-01-11 00:21:33 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,7 +18,7 @@ global.antidelete = true
 global.autotyping = false
 global.autoBio = true
 global.antiSpam = true
-global.sign = "😝🍑"
+global.sign = "🍆🍑"
 // ☞ ➻ ➽ ☛ 〆 
 global.bank = "Opay"
 global.bankname = "BLUE"
@@ -41,7 +41,7 @@ global.wm = "𝐁𝐋𝐔𝐄𝐗𝐃𝐄𝐌𝐎𝐍 𝐕𝟒"
 global.fake = botName
 global.setmenu = "image" 
 global.docType = "application/vnd.ms-excel"
-global.themeemoji = '❄️'
+global.themeemoji = '🤖'
 global.fotoRandom = [
 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbBBpaPJdt2V0Hab-7vJjlwr2NInRZ0hb6nA&usqp=CAU",
 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_KpekxrXL95vx4t9Ur6n4NRxxcrLAZWE-mA&usqp=CAU"]
